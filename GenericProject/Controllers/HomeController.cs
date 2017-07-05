e9346f6d257499defb741ec7bea4f47f5c2376f3
stephane zaffran <stephane.zaffran@gmail.com>
2017-07-05 10:17:39 +0300

    Add project files.

---
+++ GenericProject/Controllers/HomeController.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using GenericProject.Jobs;
+
+namespace GenericProject.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            //Jobs.MaListeGenerique<string> a = new MaListeGenerique<string>();
+            bool val = Jobs.MaListeGenerique<string>.AreEqual("aaa", "aaa");
+            val = Jobs.MaListeGenerique<int>.AreEqual(2, 2);
+            val = Jobs.MaListeGenerique<int>.AreEqual(2, 3);
+            val = Jobs.MaListeGenerique<string>.AreEqual("aaa", "bbb");
+            return View();
+        }
+
+        public JsonResult areEqual()
+        {
+
+            return null;
+        }
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
---
+++ GenericProject/Jobs/MaListeGenerique.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace GenericProject.Jobs
+{
+    public  class MaListeGenerique<T>
+    {
+        private int capacite;
+        private int nbElements;
+        private T[] tableau;
+
+
+        public MaListeGenerique()
+        {
+            capacite = 10;
+            nbElements = 0;
+            tableau = new T[capacite];
+        }
+
+
+        public void Ajouter(T element)
+        {
+            if (nbElements >= capacite)
+            {
+                capacite *= 2;
+                T[] copieTableau = new T[capacite];
+                for (int i = 0; i < tableau.Length; i++)
+                {
+                    copieTableau[i] = tableau[i];
+                }
+                tableau = copieTableau;
+            }
+            tableau[nbElements] = element;
+            nbElements++;
+        }
+
+        public static bool AreEqual(T x, T y)
+        {
+            return x.Equals(y);
+        }
+
+    }
+}
+
